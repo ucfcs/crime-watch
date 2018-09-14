19ac80511219dde4aa702e9ae20a8efa20e0ea33
Nick Deleuze <deleuze.nicholas@knights.ucf.edu>
2018-09-14 13:40:21 -0400

    Reports Map now shows the users current location with a market and a description of latitude/longitude

--- mobile/app/modules/home/scenes/Map/Map.js
+++ mobile/app/modules/home/scenes/Map/Map.js
@@ -1,25 +1,54 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import {connect} from 'react-redux';
 
 class Map extends React.Component {
-    constructor(){
-        super();
-        this.state = { }
+    constructor(props){
+        super(props);
+
+        this.state = {
+            latitude: 0.0,
+            longitude: 0.0,
+            error: null
+        }
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    error: null
+                });
+            },
+            (error) => this.setState({ error: error.message }),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+        );
     }
 
     render() {
         return (
             <View style={styles.container}>
+            <Text>Latitude: {this.state.latitude}</Text>
+            <Text>Longitude: {this.state.longitude}</Text>
                 <MapView style={styles.map}
                 region={{
-                    latitude: 28.6024,
-                    longitude: -81.2000,
+                    latitude: this.state.latitude,
+                    longitude: this.state.longitude,
                     latitudeDelta: 0.08,
                     longitudeDelta: 0.08
                 }}
                 >
+                    <MapView.Marker
+                        coordinate={{
+                            latitude: this.state.latitude,
+                            longitude: this.state.longitude
+                        }}
+                        title={"Current Position"}
+                        description={this.state.latitude + " " + this.state.longitude}    
+                    />
                 </MapView>
                 
             </View>
