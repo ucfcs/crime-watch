8a6571bec70d4847c03943ee0eaa1b2ccfe599c3
Henry T <ton.henry92@gmail.com>
2018-09-16 00:49:10 -0400

    Add files via upload

--- mobile/app/modules/home/actions.js
+++ mobile/app/modules/home/actions.js
@@ -1,18 +1,53 @@
 import * as t from './actionTypes';
 import * as api from './api';
-import { auth } from "../../config/firebase";
 
-import { AsyncStorage } from 'react-native';
-
-export function getUserInfo (callback)
+export function changeGender (user, gender)
 {
     return (dispatch) =>
     {
-        api.getUser(user, function (success, data, error)
+        api.changeGender(user, gender, function (success, data, error) 
         {
-
+            if (success)
+            {
+                dispatch({type: t.CHANGE_GENDER, data: gender});
+                console.log("State change done");
+            }
+            else if (error)
+                console.log("error");
         });
     };
 }
+
+export function changePhone (user, phone)
+{
+    return (dispatch) =>
+    {
+        api.changePhone(user, phone, function (success, data, error) 
+        {
+            if (success)
+            {
+                dispatch({type: t.CHANGE_PHONE, data: phone});
+                console.log("State change done");
+            }
+            else if (error)
+                console.log("error");
+        });
+    };
+}
+
+export function getData (phone)
+{
+    return (dispatch) =>
+    {
+        api.getData(phone, function (success, report)
+        {
+            if (success)
+                dispatch({type: t.DATA_RECEIVED, data: report})
+            else
+                console.log("error");
+        });
+    };
+}
+
 
 
--- mobile/app/modules/home/api.js
+++ mobile/app/modules/home/api.js
@@ -1,4 +1,3 @@
-
 import { auth, database, provider } from "../../config/firebase";
 
 
