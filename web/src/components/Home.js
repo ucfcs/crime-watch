bd2c590623611a260b0b23cc5d267470ad84a08d
jasontaylor <jasont111@yahoo.com>
2018-11-05 19:06:51 -0500

    more bootstrapping

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -6,6 +6,7 @@ import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import { db } from '../firebase';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import { getReports } from '../firebase/db';
+import { VictoryBar, VictoryPie } from 'victory';
 
 let order = 'desc';
 
@@ -63,9 +64,11 @@ class HomePage extends Component {
   render() {
     return (
     <div className="content">
-      <h1 class="text-center">
-      <Label bsStyle="danger">REPORT LIST</Label>
+      <h1 class="text-center" style={ {fontFamily:'Garamond'} }>
+      <Label bsStyle="danger">Report List</Label>
       </h1>
+      <br/>
+
       <BootstrapTable ref='table' data={ this.state.reportList }>
       <TableHeaderColumn dataField='date' width='150' isKey={ true } dataSort={ true }>Date</TableHeaderColumn>
       <TableHeaderColumn dataField='time' width='150' dataSort={ true }>Time</TableHeaderColumn>
@@ -73,8 +76,29 @@ class HomePage extends Component {
       <TableHeaderColumn dataField='latitude' width='150' dataSort={ true }>Latitude</TableHeaderColumn>
       <TableHeaderColumn dataField='longitude' width='150' dataSort={ true }>Longitude</TableHeaderColumn>
       </BootstrapTable>
+
+      <h1 class="text-center" style={ {fontFamily:'Garamond'} }>
+      <Label bsStyle="primary">Data Visualization</Label>
+      </h1>
+      <br/>
+      <div style={ {height:'400px', width:'1500px', display:'inline-block', backgroundColor:'#D6EAF8'} }>
+        <div style={{marginLeft:'100px', float:'left'}}>
+          <VictoryBar/>
+        </div>
+        <div style={{marginLeft:'300px', float:'left'}}>
+          <VictoryPie
+            colorScale={["tomato", "orange", "gold", "cyan", "navy"]}
+          />
+        </div>
+      </div>
+      <br/>
+
+      <h1 class="text-center" style={ {fontFamily:'Garamond'} }>
+      <Label bsStyle="success">Incident Map</Label>
+      </h1>
+      <br/>
       <div>
-        <Map  style={{height: '70%'}}
+        <Map  style={ {height: '70%'} }
               google={this.props.google}
               center={{
                 lat: 28.602571,
@@ -92,6 +116,7 @@ class HomePage extends Component {
               }
         </Map>
       </div>
+      <br/>
     </div>
     );
   }
